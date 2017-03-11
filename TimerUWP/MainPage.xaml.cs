ced64bdfcaf8663e5c208f7bf1e393fdeb10e5ba
rubgithub <rubensatual@gmail.com>
2017-03-10 23:00:45 -0400

    Add project files.

---
+++ TimerUWP/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+using Windows.ApplicationModel.Activation;
+using Windows.ApplicationModel.Background;
+
+namespace TimerUWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            RegisterTask();
+        }
+
+        private void RegisterTask()
+        {
+            //create a task/timer to run each 15 minutes (minimum allowed 15min)
+            var builder = new BackgroundTaskBuilder
+            {
+                Name = "My Background TimerTrigger"
+            };
+            builder.SetTrigger(new TimeTrigger(15, false));
+            builder.Register();
+            Debug.WriteLine($"RegisterTask {DateTime.Now}");
+        }
+    }
+
+    sealed partial class App 
+    {
+        protected override void OnBackgroundActivated(BackgroundActivatedEventArgs args)
+        {
+            //Can be fired more than once per trigger, check it out.
+            Debug.WriteLine($"OnBackgroundActivated - {DateTime.Now}");
+        }
+    }
+}
